faa5d999e07cf5a72a9dba66a26f5276d7ef1240
Andy <andy.schobo@gmail.com>
2018-10-16 11:15:21 -0500

    Update survival.js

--- survival.js
+++ survival.js
@@ -12,17 +12,30 @@
  * 5 slot inventory (although 10 slots displayed on screen
  * @TODO:
  * Add comments on current code
- * Make collide function COMPLETE
+ * Make collide function
  * Make other inanimate objects
  * Other features that we need to add...
 ***/
-textFont(createFont("Candara"), 15);
-//Uninitialized variables for the Player and the camera (so things don't throw errors at us)
+
+/**
+ * Key - (index of array is its id, deprecated id array)
+ * 0 = wood
+ * 1 = berry (WIP)
+ * 2 = stone (WIP)
+ * 3 = crafting box (WIP)
+ *
+ * */
+
+//@GLOBAL VARIABLES
+{
 var Player;
 var cam;
-//A variable defining whether the map is shown
 var mapsize = 4000;
 var keys = [];
+textFont(createFont("Candara"), 15);
+}
+//@MAP GENERATION
+{
 var xoff = round(random(0, 4000));
 var sum = 0;    
 var grid = [];
@@ -38,20 +51,54 @@ for (var x = 0; x < 100; x ++) {
     xoff += 0.02;
 }
 var avg = sum/10000;
-//@key interaction
+}
+
+//@KEY INTERACTION
 keyPressed = function(){keys[keyCode]=true;};
 keyReleased = function(){ keys[keyCode]=false; };
 
-/**
- * Key - (index of array is its id, deprecated id array)
- * 0 = wood
- * 1 = berry (WIP)
- * 2 = stone (WIP)
- * 3 = crafting box (WIP)
- *
- * */
+/***
+ * Basic camera function: A function to have a “camera” follow
+ * the player’s field of view.
+ * @param x the x-position of the camera
+ * @param y the y-position of the camera
+ * @func view translate field of view to the camera
+***/
+var Camera = function(x, y) {
+    // Variables about where the camera is
+    this.x=x; this.y=y; this.w=width; this.h=height;
+    /***
+      * Basic view function: Moves the screen to focus on the object plyer
+      * by translation. Will not move past the edge of map (0,0) upper left to 
+      * (4000, 4000) lower right.
+      * @param plyer an object that the camera is following which has x and y
+    ***/
+    this.view = function(plyer){
+        this.x=plyer.x;
+        this.y=plyer.y;
+        
+        this.x = constrain(this.x,this.w/2 - 100,mapsize-this.w/2 + 100);
+        this.y = constrain(this.y,this.h/2 - 100,mapsize-this.h/2 + 100);
+        translate(width/2-this.x,height/2-this.y);
+    };
+};
 
-var obj_count = [0, 0, 0, 0, 0]; //Displayed in reverse order (sorry but that's the best way it could be designed
+/***
+ * Simple function to find if the object is in view of the camera.
+ * @param obj the object that is to be analysed
+***/
+var view = function(obj){
+    return obj.x+width/2-cam.x<width&&obj.x+width/2-cam.x>-obj.r&&
+    obj.y+height/2-cam.y<height&&obj.y+height/2-cam.y>-obj.r;
+};
+
+//Radial based collide function
+var collide = function(obj1, obj2) {
+    return dist(obj1.x, obj1.y, obj2.x, obj2.y) < obj1.r/2 + obj2.r/2;
+};
+
+//Displayed in reverse order (sorry but that's the best way it could be designed
+var obj_count = [0, 0, 0, 0, 0]; 
 
 /** Lynette's Inventory*/
 var Inventory = function(x, y) {
@@ -78,97 +125,39 @@ var Inventory = function(x, y) {
     };
 };
 
-
 var inventory = new Inventory(500, 550);
 
 
-/***
- * Basic camera function: A function to have a “camera” follow
- * the player’s field of view.
- * @param x the x-position of the camera
- * @param y the y-position of the camera
- * @func view translate field of view to the camera
-***/
-var Camera = function(x, y) {
-    // Variables about where the camera is
-    this.x=x; this.y=y; this.w=width; this.h=height;
-    /***
-      * Basic view function: Moves the screen to focus on the object plyer
-      * by translation. Will not move past the edge of map (0,0) upper left to 
-      * (4000, 4000) lower right.
-      * @param plyer an object that the camera is following which has x and y
-    ***/
-    this.view = function(plyer){
-        this.x=plyer.x;
-        this.y=plyer.y;
-        
-        this.x = constrain(this.x,this.w/2,4000-this.w/2);
-        this.y = constrain(this.y,this.h/2,4000-this.h/2);
-        translate(width/2-this.x,height/2-this.y);
-    };
-};
-
-/***
- * Simple function to find if the object is in view of the camera.
- * @param obj the object that is to be analysed
-***/
-var view = function(obj){
-    return obj.x+width/2-cam.x<width&&obj.x+width/2-cam.x>-obj.w&&
-    obj.y+height/2-cam.y<height&&obj.y+height/2-cam.y>-obj.h;
-};
-
-
 //Reference points for player testing
-var referencepoint = function(x, y) {
-this.x = x;
+var tree = function(x, y) {
+    this.x = x;
     this.y = y;
-    this.w = 40;
-    this.h = 40;
     this.r = 40;
-    this.leaves = random(4, 5);
-    this.leafmult = this.leaves/4;
-    this.leafpositions = [];
-    for(var i = 0; i < this.leaves*3; i++) {
-        this.leafpositions[i] = random(15, 20);
-    }
+    this.leaves = random(3, 5);
     this.draw = function() {
         noStroke();
-        fill(11, 120, 18);
-        ellipse(this.x, this.y, this.r*this.leafmult*14/4, this.r*this.leafmult*14/4);
-        pushMatrix();
-        translate(this.x, this.y);
-        for(var i = 0; i < this.leafpositions.length; i++) {
-            rotate(120/this.leaves);
-            ellipse(this.leafpositions[i]*this.leaves, -3*this.leaves, this.leafmult*this.r, this.leafmult*this.r);
-        }
-        popMatrix();
+        fill(11, 120, 18, 100);
+        ellipse(this.x, this.y, this.r*this.leaves, this.r*this.leaves);
         fill(92, 46, 4);
         ellipse(this.x, this.y, this.r, this.r);
     };
     this.harvest = function()  {
-        this.w -= (40/this.leaves);
-        this.h -= (40/this.leaves);
-        this.r -= (40/this.leaves);
-        for(var i = 0; i < this.leaves*3; i++) {
-            this.leafpositions[i] *= this.w / (this.w + 40/this.leaves);
-        }
+        this.r -= 8;
     };
 };
-var refpoints = [];
-refpoints.add = function(x, y) {
-    refpoints.push(new referencepoint(x, y));
+var trees = [];
+trees.add = function(x, y) {
+    trees.push(new tree(x, y));
 };
-refpoints.apply = function() {
-    for(var i = 0; i < refpoints.length; i++) {
-        refpoints[i].draw();
+trees.apply = function() {
+    for(var i = 0; i < trees.length; i++) {
+        trees[i].draw();
     }
 };
 
 var bush = function(x, y) {
     this.x = x;
     this.y = y;
-    this.w = 40;
-    this.h = 40;
     this.r = 40;
     this.berries = [[this.x+8, this.y-4], [this.x-23, this.y+1],[this.x-11, this.y-17], [this.x-1, this.y+10]];
     this.draw = function() {
@@ -176,13 +165,13 @@ var bush = function(x, y) {
             noStroke();
             fill(19, 145, 21);
              if (this.berries.length !== 0){
-            ellipse(this.x, this.y, this.w, this.h);
-            ellipse(this.x-20, this.y, this.w*(4/5), this.h*0.8);
-            ellipse(this.x-9, this.y-15, this.w*0.7, this.h*0.7);
+            ellipse(this.x, this.y, this.r, this.r);
+            ellipse(this.x-20, this.y, this.r*(4/5), this.r*0.8);
+            ellipse(this.x-9, this.y-15, this.r*0.7, this.r*0.7);
             
             for (var i = 0; i < this.berries.length; i++){
                fill(247, 15, 15);
-               ellipse(this.berries[i][0], this.berries[i][1], this.w/5, this.h/5);
+               ellipse(this.berries[i][0], this.berries[i][1], this.r/5, this.r/5);
             }
              }
         }
@@ -207,8 +196,7 @@ var player = function(x, y) {
     this.y = y;
     this.xspeed = 0;
     this.yspeed = 0;
-    this.w = 20;
-    this.h = 20;
+    this.r = 20;
     this.dir = atan2(this.y - mouseY, mouseX - this.x);
     this.speedLimit = 3;
     this.draw = function() {
@@ -217,8 +205,8 @@ var player = function(x, y) {
         pushMatrix();
         translate(this.x, this.y);
         rotate(this.dir);
-        ellipse(0, 0, 20, 20);
-        rect(0, 6, 20, 5, 2);
+        ellipse(0, 0, this.r, this.r);
+        rect(0, 6, this.r, 5, 2);
         popMatrix();
         fill(0);
     };
@@ -236,10 +224,15 @@ var player = function(x, y) {
         if(keys[RIGHT] || keys[68]) {
             this.xspeed += 0.1;
         }
-        this.y += this.yspeed;
+        println(this.xspeed);
         this.x += this.xspeed;
-        this.xspeed *= 0.93;
-        this.yspeed *= 0.93;
+        this.applyCollision(trees, this.xspeed, 0);
+        this.y += this.yspeed;
+        this.applyCollision(trees, 0, this.yspeed);
+        this.x = constrain(this.x, this.r/2, mapsize - this.r/2);
+        this.y = constrain(this.y, this.r/2, mapsize - this.r/2);
+        this.xspeed *= 0.9;
+        this.yspeed *= 0.9;
         this.dir = atan2(mouseY - height/2, mouseX - width/2);
         this.xspeed = constrain(this.xspeed, -1*this.speedLimit, this.speedLimit);
         this.yspeed = constrain(this.yspeed, -1*this.speedLimit, this.speedLimit);
@@ -260,20 +253,30 @@ var player = function(x, y) {
         text("Location: (" + round(this.x*100)/100 + ", " + round(this.y*100)/100 + ")", 20, 20);
         text("Facing: " + round((((-1*Math.sign(this.dir)+1)/2)*360 + this.dir)*100)/100 + " degrees from East", 20, 35);
     };
-    this.collectTree = function(refpoint){
-        return abs(this.x - refpoint.x)*2 < (refpoint.w*refpoint.leaves) && abs(this.y - refpoint.y)*2 < (refpoint.w*refpoint.leaves);
+    this.collectTree = function(tree){
+        return abs(this.x - tree.x)*2 < (tree.r*tree.leaves) && abs(this.y - tree.y)*2 < (tree.r*tree.leaves);
     };
     this.collectBush = function(bush){
         return abs(this.x - bush.x)*2 < (bush.r * 1.5) && abs(this.y - bush.y)*2 < (bush.r * 1.5);
     };
+    this.applyCollision=function(obj,velx,vely){
+        for(var i=0; i<obj.length; i++){
+            if(collide(this,obj[i])){ // handle collisions
+                if(vely>0){ this.yvel=0; this.y=obj[i].y-this.r; }
+                if(vely<0){ this.yvel=0; this.y=obj[i].y+obj[i].r; }
+                if(velx<0){ this.xvel=0; this.x=obj[i].x+obj[i].r; }
+                if(velx>0){ this.xvel=0; this.x=obj[i].x-this.r; }
+            }
+        }
+    };
 };
-Player = new player(2000, 2000);
+Player = new player(200, 200);
 cam = new Camera(Player.x, Player.y);
 
 mouseClicked = function() {
-    for (var i = 0; i < refpoints.length; i++) {
-        if (Player.collectTree(refpoints[i]) === true) {
-            refpoints[i].harvest();
+    for (var i = 0; i < trees.length; i++) {
+        if (Player.collectTree(trees[i]) === true) {
+            trees[i].harvest();
             obj_count[0]++;
         }
     }
@@ -285,7 +288,7 @@ mouseClicked = function() {
     }
 };
 
-for(var i = 0; i < 150; i++) {
+for(var i = 0; i < 450; i++) {
     var x = random(0, 4000);
     var y = random(0, 4000);
     var position = round(x/40)*100 + round(y/40);
@@ -294,11 +297,11 @@ for(var i = 0; i < 150; i++) {
         y = random(0, 4000);
         position = round(x/40)*100 + round(y/40);
     }
-    refpoints.add(x, y);  
+    trees.add(x, y);  
 }
         
 
-for(var i = 0; i < 50; i++) {
+for(var i = 0; i < 150; i++) {
     var a = random(0, 4000);
     var b = random(0, 4000);
     var position = round(a/40)*100 + round(b/40);
@@ -307,15 +310,9 @@ for(var i = 0; i < 50; i++) {
         b = random(0, 4000);
         position = round(a/40)*100 + round(b/40);
     }
-    bushes.add(a,b);  
+    bushes.add(a,b);
 }
 
-for(var i = 0; i < 100; i++) {
-    for(var j = 0; j < 100; j++) {
-        fill(grid[i*100 + j]);
-        rect(i*5, j*5, 5, 5);
-    }
-}
 
 var scene = 0;
 var draw = function() {
@@ -324,9 +321,14 @@ var draw = function() {
         rectMode(CORNER);
         pushMatrix();
         cam.view(Player);
-        refpoints.apply();
+        stroke(255, 0, 0);
+        line(0, 0, mapsize, 0);
+        line(0, 0, 0, mapsize);
+        line(mapsize, 0, mapsize, mapsize);
+        line(0, mapsize, mapsize, mapsize);
         bushes.apply();
         Player.draw();
+        trees.apply();
         Player.update();
         popMatrix();
         Player.stats();
